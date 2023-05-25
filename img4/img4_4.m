1e5dc377645f0dcb1a286c80b2928d2181cd5098
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2023-05-25 18:29:36 +0900

    img4_4 add filter

--- img4/img4_4.m
+++ img4/img4_4.m
@@ -3,43 +3,69 @@ close all;
 
 img=imread('LENNA.bmp');
 [h,w]=size(img);
-filter=ones(h,w);
+filter1=ones(h,w);
+filter2=ones(h,w);
 
 x=w/2; y=h/2;
 for k=1:h
     for l=1:w
         d=sqrt((y-k)^2 + (x-l)^2);
+        if (d <= 50)
+            filter2(k,l)=0;
+        end
         if (d <= 100)
-            filter(k,l)=0;
+            filter1(k,l)=0;
         end
     end
 end
 
 img_f=fft2(img);
 img_fs=fftshift(img_f);
-img_fil=filter.*img_fs;
-img_ifs=ifftshift(img_fil);
-img_if=ifft2(img_ifs);
-result=real(img_if);
+img_fil1=filter1.*img_fs;
+img_fil2=filter2.*img_fs;
+img_ifs1=ifftshift(img_fil1);
+img_ifs2=ifftshift(img_fil2);
+img_if1=ifft2(img_ifs1);
+img_if2=ifft2(img_ifs2);
+result1=real(img_if1);
+result2=real(img_if2);
 
 figure;
 colormap(gray(256));
-imagesc(result);
+imagesc(result1);
 xlabel('x周波数');
 ylabel('y周波数');
+title('100画素消去');
+
+figure;
+colormap(gray(256));
+imagesc(result2);
+xlabel('x周波数');
+ylabel('y周波数');
+title('50画素消去');
 
 img_fs_abs=abs(img_fs);
-img_fil_abs=abs(img_fil);
+img_fil1_abs=abs(img_fil1);
+img_fil2_abs=abs(img_fil2);
 power1=log(1+img_fs_abs.^2);
-power2=log(1+img_fil_abs.^2);
+power2=log(1+img_fil1_abs.^2);
+power3=log(1+img_fil2_abs.^2);
+
 figure;
 colormap(gray(256));
 imagesc(power1);
 xlabel('x周波数');
 ylabel('y周波数');
+title('img\_power');
 figure;
 colormap(gray(256));
 imagesc(power2);
 xlabel('x周波数');
 ylabel('y周波数');
-
+title('filterd1\_power');
+figure;
+colormap(gray(256));
+imagesc(power3);
+xlabel('x周波数');
+ylabel('y周波数');
+title('filterd2\_power');
